da25974ee828aedd43c0dc8a7e648d4cc7835ac9
cogdotnet129 <cogdotnet129@CTSDOTNET290>
2022-08-02 15:29:46 +0530

    Add project files.

---
+++ EStocks/Model/CompanyEStock.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EStocks.Model
+{
+    public class CompanyEStock
+    {
+        [Key]
+        public int CompanyStockId { get; set; }
+        public int CompanyId { get; set; }
+        public decimal StockPrice { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        //public string CompanyCode { get; set; }
+    }
+}
